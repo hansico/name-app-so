0947b8f7fb14c52a5b87e0f17b7c04afb0fe852a
hansico <75609606+hansico@users.noreply.github.com>
2021-01-18 20:36:59 +0200

    change api routes, request parameter checks, sorting

--- serv.py
+++ serv.py
@@ -1,20 +1,60 @@
-from flask import Flask
+from flask import Flask, request
 import json
 
 DB = "./data/names.json"
 
 app = Flask(__name__)
 
-def accessDB(DB):
+def read_db():
   f = open(DB,'r')
-  data = f.read()
+  data = json.loads(f.read())['names']
   f.close()
-  return json.loads(data)
+  return data
+
+def query_from_db(keys=['name','amount'],sort=None,reverse=False):
+  data = read_db()
+  if sort:
+    data.sort(key=lambda x: x[sort], reverse=reverse)
+  data = [ [x[k] for k in keys] for x in data ]
+  return data
 
 @app.route('/api/names', methods=['GET'])
 def names_api():
-  data = accessDB(DB)
+  # Defaults
+  sort, reverse = None, False
+  keys = ['name']
+
+  allowed_args = ['sort','reverse','keys']
+  allowed_keys = ['amount','name']
+
+  for arg in request.args:
+    if arg not in allowed_args:
+      return "Unrecognized parameter: "+arg, 200
+
+  if 'keys' in request.args:
+    keys = request.args['keys'].split("|")
+    for key in keys:
+      if key not in allowed_keys:
+        return "Unrecognized key: "+request.args[arg], 200
+
+  if 'sort' in request.args:
+    if request.args['sort'] not in allowed_keys:
+      return "Unrecognized sort: "+request.args[arg], 200
+    sort = request.args['sort']
+    
+    if 'reverse' in request.args:
+      if request.args['reverse'] not in ["True","False"] :
+        return "Invalid value for reverse: "+request.args['reverse']+". Should be True or False",200
+      reverse = request.args['reverse'] == 'True'
+    
+  data = query_from_db(keys=keys,sort=sort,reverse=reverse)
+  data = str(data)
+
   return data, 200
+
+@app.route('/api/names/count')
+def count_api():
+  return "Not Implemented",501
 
 if __name__ == '__main__':
   app.run()
